ef91a57ffa0fa1b8383090f30f20caf5d91cdd0c
Daniel Chen <daniel.kitaharasupergeeks@gmail.com>
2026-06-19 19:04:47 -0300

    cinemachine e playercamera
    19/06/2026

--- Assets/scripts/movePlayer.cs
+++ Assets/scripts/movePlayer.cs
@@ -18,26 +18,34 @@ public class movePlayer : MonoBehaviour
     [SerializeField] private float gravity = -9.81f;
 
     private CharacterController controller;
-
     private float verticalVelocity;
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+
+        if (moveAction == null)
+            Debug.LogError("Move Action não foi atribuída!", this);
+
+        if (sprintAction == null)
+            Debug.LogError("Sprint Action não foi atribuída!", this);
+
+        if (jumpAction == null)
+            Debug.LogError("Jump Action não foi atribuída!", this);
     }
 
     private void OnEnable()
     {
-        moveAction.action.Enable();
-        sprintAction.action.Enable();
-        jumpAction.action.Enable();
+        moveAction?.action.Enable();
+        sprintAction?.action.Enable();
+        jumpAction?.action.Enable();
     }
 
     private void OnDisable()
     {
-        moveAction.action.Disable();
-        sprintAction.action.Disable();
-        jumpAction.action.Disable();
+        moveAction?.action.Disable();
+        sprintAction?.action.Disable();
+        jumpAction?.action.Disable();
     }
 
     private void Update()
@@ -48,19 +56,34 @@ public class movePlayer : MonoBehaviour
 
     private void HandleMovement()
     {
+        if (moveAction == null)
+            return;
+
         Vector2 input = moveAction.action.ReadValue<Vector2>();
 
-        bool isSprinting = sprintAction.action.IsPressed();
+        bool isSprinting =
+            sprintAction != null &&
+            sprintAction.action.IsPressed();
 
-        float currentSpeed = isSprinting
-            ? sprintSpeed
-            : walkSpeed;
+        float currentSpeed =
+            isSprinting ? sprintSpeed : walkSpeed;
 
         Vector3 moveDirection =
             transform.right * input.x +
             transform.forward * input.y;
 
-        controller.Move(moveDirection * currentSpeed * Time.deltaTime);
+        controller.Move(
+            moveDirection *
+            currentSpeed *
+            Time.deltaTime
+        );
+
+#if UNITY_EDITOR
+        if (input != Vector2.zero)
+        {
+            Debug.Log($"Input: {input}");
+        }
+#endif
     }
 
     private void HandleJumpAndGravity()
@@ -70,17 +93,26 @@ public class movePlayer : MonoBehaviour
             if (verticalVelocity < 0f)
                 verticalVelocity = -2f;
 
-            if (jumpAction.action.WasPressedThisFrame())
+            if (
+                jumpAction != null &&
+                jumpAction.action.WasPressedThisFrame()
+            )
             {
                 verticalVelocity =
-                    Mathf.Sqrt(jumpHeight * -2f * gravity);
+                    Mathf.Sqrt(
+                        jumpHeight *
+                        -2f *
+                        gravity
+                    );
             }
         }
 
         verticalVelocity += gravity * Time.deltaTime;
 
         controller.Move(
-            Vector3.up * verticalVelocity * Time.deltaTime
+            Vector3.up *
+            verticalVelocity *
+            Time.deltaTime
         );
     }
 }
